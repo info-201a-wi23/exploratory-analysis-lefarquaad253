17b3a5a42a5cef2e8f2d38fcd404a28457e262af
nalinudomsak <nalinu@uw.edu>
2023-02-16 13:22:38 -0800

    progress 3 on bar chart

--- Chart3_nalin.R
+++ Chart3_nalin.R
@@ -2,6 +2,7 @@
 library("dplyr")
 library("stringr")
 library("ggplot2")
+library("scales")
 
 allDogDescriptions <- read.csv("allDogDescriptions.csv", stringsAsFactors = TRUE)
 age_senior <- allDogDescriptions %>% 
@@ -13,10 +14,21 @@ age_baby <- allDogDescriptions %>%
 age_young <- allDogDescriptions %>% 
   filter(str_detect(age, "Young")) 
 
-df_sum <- c(nrow(age_baby), nrow(age_young), nrow(age_adult), nrow(age_senior))
+sum_baby <- nrow(age_baby)
+sum_young <- nrow(age_young)
+sum_adult <- nrow(age_adult)
+sum_senior <- nrow(age_senior)
+
+df_sum <- c(sum_baby, sum_young, sum_adult, sum_senior)
 df_name <- c("Baby", "Young", "Adult", "Senior")
 df <- cbind(df_name, df_sum)
 df <- data.frame(df)
+df <- df[order(df$df_sum),]
 
-ggplot(df, aes(x=df_name, y=df_sum)) + 
-  geom_bar(stat = "identity")
+#ggplot(df, aes(x=df_name, y=df_sum)) + 
+  #geom_bar(stat = "identity")
+
+ggplot(df)+
+  geom_col(aes(x = df_name, y = df_sum))
+ 
+
