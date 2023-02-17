df78118a9789c450cde72560517042362df24cea
ImChantria <chantria@uw.edu>
2023-02-17 11:02:46 -0800

    map progress -chantria

--- chart1_chantria.R
+++ chart1_chantria.R
@@ -1,23 +1,49 @@
-#Map of the US showing amount of dogs adopted
+#Map of the US showing amount of adoptable dogs in each state
 
 
 #Load libraries
 library("ggplot2")
 library("maps")
-library("stringr")
 library("dplyr")
+library("plotly")
+library("stringr")
+library("tidyverse")
 
 # Load data
-allDogDescriptions <- read.csv("allDogDescriptions.csv", stringsAsFactors = TRUE)
+allDogDescriptions <- read.csv("C:/Users/ryuta/Desktop/INFO 201 Directory/exploratory-analysis-lefarquaad253/allDogDescriptions.csv", stringsAsFactors = TRUE)
+
 
 # Load state shapefile
 state_shape <- map_data("state")
+View(state_shape)
 
-#Map every state
-ggplot(data = state_shape) +
-  #Map each state as a group
-  geom_polygon(aes(x = long,
-                   y = lat,
-                   group = group))
-          
-  coord_map()
+# Add full (lowercased) state name to allDogDescriptions, matching on state abbreviation
+allDogDescriptions <- allDogDescriptions %>% 
+  mutate(state_fullname =
+           tolower(state.name[match(contact_state, state.abb)]))
+
+# Make a new dataframe of the total adoptable dogs for each state (by full name)
+adoption_data <- allDogDescriptions %>%  
+  group_by(state_fullname) %>% 
+  summarize(state_total = n()) %>%
+  drop_na(state_fullname)
+
+
+# Join the `state_shape` and `allDogsDescriptions` dataframes as
+# state_shape_data
+state_shape_data <- left_join(state_shape, 
+                                 adoption_data,
+                                 by = c("region" = "state_fullname"))
+
+# Make a choropleth map of amount of adoptable dogs by state
+ggplot(state_shape_data) +
+  geom_polygon(mapping = aes(x = long, 
+                             y = lat, 
+                             group = group, 
+                             fill = state_total)) +
+  coord_map() +
+  labs(title = 'Amount of Adoptable Dogs in Each State', fill = 'Amount')
+
+
+
+
